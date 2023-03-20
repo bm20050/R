df27cae57e8021de7c87bd7b1590f87f6d5f9554
bm20050 <bm20050@gmail.com>
2023-03-20 21:49:57 +0900

    03.20 update

--- homework/hw3.R
+++ homework/hw3.R
@@ -1,7 +1,6 @@
 
 # 7장
 
-
 # 2번
 dataset <- read.csv("./data/dataset.csv", header=T)
 dataset
@@ -15,7 +14,6 @@ sum(dataset2$resident)
 resident2 <- na.omit(dataset2$resident)
 sum(resident2)
 
-
 # 3번
 summary(dataset2$gender)
 dataset2$gender2[dataset2$gender == 1] <- '남자'
@@ -23,7 +21,6 @@ dataset2$gender2[dataset2$gender == 2] <- '여자'
 head(dataset2[c("gender", "gender2")])
 table(dataset2$gender2)
 pie(table(dataset2$gender2))
-
 
 # 4번
 # 1은 30세 미만, 2는 30세 이상 55세 미만, 3은 55세 이상으로 변경하였습니다.
@@ -36,5 +33,28 @@ dataset2[c("age", "age3")]
 
 # 8장
 
-
 # 1번
+install.packages("lattice")
+library(lattice)
+head(quakes)
+str(quakes)
+
+depthgroup <- equal.count(quakes$depth, number = 3, overlap = 0)
+depthgroup
+
+maggroup <- equal.count(quakes$mag, number = 2, overlap = 0)
+maggroup
+
+xyplot(lat ~ long | depthgroup * maggroup, data = quakes,
+       main = "Fiji Earthquakes",
+       ylab = "latitude", xlab="longitude",
+       pch = "@", col = c("red", "blue"))
+
+# 2번
+install.packages("latticeExtra")
+library(latticeExtra)
+data(SeatacWeather)
+head(SeatacWeather)
+str(SeatacWeather)
+
+xyplot(min.temp + max.temp ~ day | month, data=SeatacWeather, type="l")
